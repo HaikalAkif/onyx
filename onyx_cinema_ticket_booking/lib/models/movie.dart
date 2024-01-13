32333281a250dd4bbc26bc763503c90c12e11d33
HaikalAkif <haikalakif17@gmail.com>
2024-01-14 00:01:01 +0800

    🐛 Class bug fixed

---
+++ onyx_cinema_ticket_booking/lib/models/movie.dart
@@ -0,0 +1,11 @@
+class Movie {
+  final String title;
+  final String imageUrl;
+  final double starRate;
+
+  Movie({
+    required this.title,
+    required this.imageUrl,
+    required this.starRate,
+  });
+}
